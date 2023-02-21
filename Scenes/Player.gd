33272870d3181e3f70a3276cf574f1de7b7e25c2
Mike Lee <mikemunkyu.lee@gmail.com>
2023-02-20 20:45:27 -0500

    Wire up animation state machine

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
 const ACCELERATION = 600
-const MAX_SPEED =  100
+const MAX_SPEED =  120
 const FRICTION = 800
 
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,16 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		if velocity.x > 0:
-			animationPlayer.play("IdleRight")
-		else:
-			animationPlayer.play("IdleLeft")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO , FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
